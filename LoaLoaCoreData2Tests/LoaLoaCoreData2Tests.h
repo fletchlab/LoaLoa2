ac522296c6d04235d50d6f009d17fb210432dae5
Mike D'Ambrosio <mdambrosio@Mikes-MacBook-Pro.local>
2012-08-22 15:09:28 -0700

    Initial Commit

---
+++ LoaLoaCoreData2Tests/LoaLoaCoreData2Tests.h
@@ -0,0 +1,13 @@
+//
+//  LoaLoaCoreData2Tests.h
+//  LoaLoaCoreData2Tests
+//
+//  Created by Mike D'Ambrosio on 8/22/12.
+//  Copyright (c) 2012 Mike D'Ambrosio. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface LoaLoaCoreData2Tests : SenTestCase
+
+@end
---
+++ LoaLoaCoreData2Tests/LoaLoaCoreData2Tests.m
@@ -0,0 +1,32 @@
+//
+//  LoaLoaCoreData2Tests.m
+//  LoaLoaCoreData2Tests
+//
+//  Created by Mike D'Ambrosio on 8/22/12.
+//  Copyright (c) 2012 Mike D'Ambrosio. All rights reserved.
+//
+
+#import "LoaLoaCoreData2Tests.h"
+
+@implementation LoaLoaCoreData2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in LoaLoaCoreData2Tests");
+}
+
+@end
